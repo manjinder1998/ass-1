2fc029e6d62e7ff4d2a9272f13948d47fbd2c10d
visualstudio <visualstudio@122-PC09>
2019-04-10 11:15:35 -0400

    ass 1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,10 +6,104 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
-    class Program
+    class ConsoleApp2
+     public JournalEntry(string note, int dist)
     {
-        static void Main(string[] args)
+        villageName = note; distanceTraveled = dist;
+        HowFarToGetBack = distanceTraveled;
+    }
+    public int HowFarToGetBack = 0;
+    private string villageName;
+    private int distanceTraveled;
+    public int getDistanceWalked() { return distanceTraveled; }
+    public string getVillageName() { return villageName; }
+}
+
+
+
+class Hugi
+{
+    private static JournalEntry je;
+    public static bool FoundAstrilde = false;
+
+    public static List<JournalEntry> HugiJournal = new List<JournalEntry>();
+
+    public static int CalculateDistanceWalked()
+    {
+        int DistanceWalked = 0;
+        // walk over the List and add the distances
+        foreach (var je in HugiJournal)
         {
+            Console.WriteLine(" {0}  --   {1} *** --- {2} ", je.getDistanceWalked(), je.getVillageName(), je.HowFarToGetBack);
+            DistanceWalked += je.getDistanceWalked() + je.HowFarToGetBack;
         }
+        return DistanceWalked;
     }
 }
+
+class CountrySide
+{
+
+    /* 
+     * 
+     */
+    static void Main()
+    {
+        CountrySide a = new CountrySide();
+        a.Run();
+    }
+
+    // Create the LinkedList to reflect the Map in the PowerPoint Instructions
+    Village Maeland;
+    Village Helmholtz;
+    Village Alst;
+    Village Wessig;
+    Village Badden;
+    Village Uster;
+    Village Schvenig;
+
+    public void TraverseVillages(Village CurrentVillage)
+    {
+        if (Hugi.FoundAstrilde) return;
+
+
+        Hugi.HugiJournal.Add(new JournalEntry(CurrentVillage.VillageName, CurrentVillage.distanceFromPreviousVillage));
+        try
+        {
+            Console.WriteLine("I am in {0}", CurrentVillage.VillageName);
+
+            if (CurrentVillage.isAstrildgeHere)
+            {
+                Console.WriteLine("I found Dear Astrildge in {0}", CurrentVillage.VillageName);
+                Console.WriteLine("**** FEELING HAPPY!!! ******");
+                Console.WriteLine("Astrilde, I walked {0} vika to find you. Will you marry me?", Hugi.CalculateDistanceWalked());
+                Hugi.FoundAstrilde = true;
+            }
+
+            TraverseVillages(CurrentVillage.west);
+            TraverseVillages(CurrentVillage.east);
+
+
+        }
+        catch (NullReferenceException) { }
+    }
+
+    public void Run()
+    {
+        Alst = new Village("Alst", false);
+        Schvenig = new Village("Schvenig", false);
+        Wessig = new Village("Wessig", false);
+        Maeland = new Village("Maeland", false);
+        Helmholtz = new Village("helmholtz", false);
+        Uster = new Village("Uster", true);
+        Badden = new Village("Badden", false);
+
+        Alst.VillageSetup(0, Schvenig, Wessig);
+        Schvenig.VillageSetup(14, Maeland, Helmholtz);
+        Maeland.VillageSetup(9, null, Helmholtz);
+        Helmholtz.VillageSetup(28, null, null);
+        Wessig.VillageSetup(19, Uster, Badden);
+        Uster.VillageSetup(28, null, null);
+        Badden.VillageSetup(11, null, null);
+
+        this.TraverseVillages(Alst);
